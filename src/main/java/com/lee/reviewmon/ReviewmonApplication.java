1f789b4ffb2c6ecceb0181dc2f39bda66b23df80
lee <seasunrise22@gmail.com>
2024-06-15 13:15:22 +0900

    create project

---
+++ src/main/java/com/lee/reviewmon/ReviewmonApplication.java
@@ -0,0 +1,13 @@
+package com.lee.reviewmon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewmonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewmonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lee/reviewmon/ReviewmonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lee.reviewmon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewmonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
